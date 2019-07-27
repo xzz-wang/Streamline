2325e4f0e1c19e9695e429d1e55070340860bf8d
Xuezheng Wang <xzzwang@icloud.com>
2019-07-26 20:16:59 -0700

    Initial Commit

---
+++ Streamline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Streamline
+//
+//  Created by Xuezheng Wang on 7/26/19.
+//  Copyright © 2019 Xuezheng Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StreamlineTests/StreamlineTests.swift
@@ -0,0 +1,34 @@
+//
+//  StreamlineTests.swift
+//  StreamlineTests
+//
+//  Created by Xuezheng Wang on 7/26/19.
+//  Copyright © 2019 Xuezheng Wang. All rights reserved.
+//
+
+import XCTest
+@testable import Streamline
+
+class StreamlineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
