c7e8837fbec2a3d501ed4ff486489e22cd2d2e0f
Xuezheng Wang <xzzwang@icloud.com>
2019-08-04 20:48:34 -0700

    Created Head.swift, the view for the head of the

---
+++ Streamline/Front/Game View/Head.swift
@@ -0,0 +1,24 @@
+//
+//  Head.swift
+//  Streamline
+//
+//  Created by Xuezheng Wang on 8/4/19.
+//  Copyright © 2019 Xuezheng Wang. All rights reserved.
+//
+
+import UIKit
+
+class Head: UIView {
+    
+    private var headColor: UIColor = .orange
+
+    override func draw(_ rect: CGRect) {
+        self.layer.isOpaque = false
+        
+        let path = UIBezierPath(ovalIn: rect)
+        
+        headColor.setFill()
+        path.fill()
+    }
+
+}
