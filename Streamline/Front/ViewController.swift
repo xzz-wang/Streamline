f43193de1172b297be519fd860c6a2b1a7e9f8c9
Bob Zhang <chz026@ucsd.edu>
2019-09-15 08:41:30 +0800

    Revert "Updated gitignore to ignore the xcuserstate file, also trying to add a level-selection View Controller Scene"
    This reverts commit 2a6d9380027168a0d7972cff8095010e3a534fb5.

--- Streamline/Front/ViewController.swift
+++ Streamline/Front/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     // Game logic delegate to back-end
     // var gameDelegate: GameLogicDelegate = TempGameDelegate()
+
     
     // Use this line when using the actual GameLogicDelegate
     var gameDelegate: GameLogicDelegate = Streamline()
@@ -27,6 +28,8 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     // Provide feedBack
     var feedbackGenerator: UIImpactFeedbackGenerator? = nil
     var levelPassedFeedbackGenerator: UINotificationFeedbackGenerator? = nil
+
+    
     
     // MARK: - View life cycles
     override func viewDidLoad() {
