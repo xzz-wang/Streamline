fc20523ce939127fe271a3cdaf1cd277db43fc8f
Xuezheng Wang <xzzwang@icloud.com>
2019-09-27 10:30:25 -0700

    CollectionView seems to be working. Now we just need to add it into the logic

--- Streamline/Front/Levels View/LevelsViewController.swift
+++ Streamline/Front/Levels View/LevelsViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class LevelsViewController: UIViewController {
+let ID = "LevelCell"
+
+class LevelsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    // MARK: - Collection View related
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ID, for: indexPath) as! LevelViewCell
+        
+        cell.levelLabel.text = String(indexPath.item + 1)
+        cell.levelNumber = indexPath.item
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
